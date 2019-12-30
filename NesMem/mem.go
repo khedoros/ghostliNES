4fadea589f5221b6cfc5fc263ef2602f4265fe13
khedoros <khedoros@davidklaptop.SLDomain.com>
2019-12-30 13:09:09 -0700

    Starting ROM loading code

--- NesMem/mem.go
+++ NesMem/mem.go
@@ -1,9 +1,21 @@
 package NesMem
 
+import (
+	"fmt"
+	"github.com/khedoros/ghostliNES/NesCart"
+)
+
 //An NesMem struct holds the state of the NES's memory mapping circuitry
 type NesMem struct {
 	Blah int8
+	cart *NesCart.NesCart
 }
 
 func (this *NesMem) New(filename *string) {
+	cart := NesCart.NesCart{}
+	fmt.Println("Loading file ", filename)
+	valid := cart.Load(filename)
+	if !valid {
+		fmt.Println("File failed to load")
+	}
 }
