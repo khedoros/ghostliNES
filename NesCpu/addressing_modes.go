ea8c3d6a9a506efa2fe461a4305369e398c540e0
khedoros <khedoros@gmail.com>
2022-06-10 02:00:24 -0600

    Reorg CPU code and implement more opcodes

--- NesCpu/addressing_modes.go
+++ NesCpu/addressing_modes.go
@@ -1,71 +1,35 @@
 package nescpu
 
-/* Read operand (if any), advance PC past the current instruction, return value of operand
-   int ind_y();
-   int zp();
-   int immediate();
-   int absa();
-   signed char relative();
-   int absa_y();
-   int absa_x();
-   int ind();
-   void imp();
-   void accum();
-*/
+import "fmt"
 
-func (cpu *CPU6502) zpX() uint16 {
-	cpu.pc += 2
-	baseAddr := cpu.mem.Read(cpu.pc-1, 0)
-	return uint16((baseAddr + cpu.xreg) & 0xFF)
+var addrUnimpl addrFunc = func(cpu *CPU6502) uint16 {
+	fmt.Print("Addressing mode unimplemented.\t")
+	return 0
 }
 
-func (cpu *CPU6502) zpY() uint16 {
-	cpu.pc += 2
-	baseAddr := cpu.mem.Read(cpu.pc-1, 0)
-	return uint16((baseAddr + cpu.yreg) & 0xFF)
+// Addressing mode implementations
+func addrIndX(cpu *CPU6502) uint16 {
+	addr := uint16((cpu.mem.Read(cpu.pc-1, cpu.cycle) + cpu.xreg) & 0xff)
+	return cpu.mem.Read16(addr, cpu.cycle)
 }
 
-func (cpu *CPU6502) indX() uint16 {
-	cpu.pc += 2
-	addr := (cpu.mem.Read(cpu.pc-1, 0) + cpu.xreg) & 0xff
-	return uint16(cpu.mem.Read(uint16(addr), 0)) | (uint16(cpu.mem.Read(uint16(addr+1), 0)) * uint16(256))
+func addrIndY(cpu *CPU6502) uint16 {
+	addr := uint16(cpu.mem.Read(cpu.pc-1, cpu.cycle))
+	return cpu.mem.Read16(addr, cpu.cycle) + uint16(cpu.yreg)
 }
 
-/*
-inline int cpu::ind_y() {
-    int addr=memory->read(pc+1);
-    pc+=2;
-    return (((memory->read(addr))|((memory->read((addr+1)&0xFF))<<(8)))+y)&0xFFFF;
+func addrIndirect(cpu *CPU6502) uint16 {
+	addr := cpu.mem.Read16(cpu.pc-2, cpu.cycle)
+	return cpu.mem.Read16(addr, cpu.cycle)
 }
 
-inline int cpu::zp() {
-    int base_addr=memory->read(pc+1);
-    pc+=2;
-    return base_addr;
-}
-
-inline int cpu::immediate() {
-    pc+=2;
-    return pc-1;
-}
-
-inline int cpu::absa() {
-    pc+=3;
-    return memory->read2(pc-2);
-}
-
-inline int cpu::absa_y() {
-    pc+=3;
-    return (memory->read2(pc-2)+y)&0xFFFF;
-}
-
-inline int cpu::absa_x() {
-    pc+=3;
-    return (memory->read2(pc-2)+x)&0xFFFF;
-}
-
-inline signed char cpu::relative() {
-    pc+=2;
-    return memory->read(pc-1);
-}
-*/
+func addrZp(cpu *CPU6502) uint16        { return uint16(cpu.mem.Read(cpu.pc-1, cpu.cycle)) }
+func addrZpX(cpu *CPU6502) uint16       { return uint16(cpu.mem.Read(cpu.pc-1, cpu.cycle) + cpu.xreg) }
+func addrZpY(cpu *CPU6502) uint16       { return uint16(cpu.mem.Read(cpu.pc-1, cpu.cycle) + cpu.yreg) }
+func addrAbs(cpu *CPU6502) uint16       { return cpu.mem.Read16(cpu.pc-2, cpu.cycle) }
+func addrAbsY(cpu *CPU6502) uint16      { return cpu.mem.Read16(cpu.pc-2, cpu.cycle) + uint16(cpu.yreg) }
+func addrAbsX(cpu *CPU6502) uint16      { return cpu.mem.Read16(cpu.pc-2, cpu.cycle) + uint16(cpu.xreg) }
+func addrImmediate(cpu *CPU6502) uint16 { return cpu.pc - 1 }
+func addrRelative(cpu *CPU6502) uint16  { return cpu.pc - 1 }
+func addrImplied(cpu *CPU6502) uint16   { return 0 }
+func addrAccum(cpu *CPU6502) uint16     { return 0 }
--- NesCpu/operations.go
+++ NesCpu/operations.go
@@ -7,35 +7,6 @@ var opUnimpl opFunc = func(cpu *CPU6502, arg uint16) int64 {
 	return 0
 }
 
-var addrUnimpl addrFunc = func(cpu *CPU6502) uint16 {
-	fmt.Print("Addressing mode unimplemented.\t")
-	return 0
-}
-
-// Addressing mode implementations
-func addrIndX(cpu *CPU6502) uint16 {
-	addr := uint16((cpu.mem.Read(cpu.pc-1, cpu.cycle) + cpu.xreg) & 0xff)
-	return cpu.mem.Read16(addr, cpu.cycle)
-}
-func addrIndY(cpu *CPU6502) uint16 {
-	addr := uint16(cpu.mem.Read(cpu.pc-1, cpu.cycle))
-	return cpu.mem.Read16(addr, cpu.cycle) + uint16(cpu.yreg)
-}
-func addrIndirect(cpu *CPU6502) uint16 {
-	addr := cpu.mem.Read16(cpu.pc-2, cpu.cycle)
-	return cpu.mem.Read16(addr, cpu.cycle)
-}
-func addrZp(cpu *CPU6502) uint16        { return uint16(cpu.mem.Read(cpu.pc-1, cpu.cycle)) }
-func addrZpX(cpu *CPU6502) uint16       { return uint16(cpu.mem.Read(cpu.pc-1, cpu.cycle) + cpu.xreg) }
-func addrZpY(cpu *CPU6502) uint16       { return uint16(cpu.mem.Read(cpu.pc-1, cpu.cycle) + cpu.yreg) }
-func addrAbs(cpu *CPU6502) uint16       { return cpu.mem.Read16(cpu.pc-2, cpu.cycle) }
-func addrAbsY(cpu *CPU6502) uint16      { return cpu.mem.Read16(cpu.pc-2, cpu.cycle) + uint16(cpu.yreg) }
-func addrAbsX(cpu *CPU6502) uint16      { return cpu.mem.Read16(cpu.pc-2, cpu.cycle) + uint16(cpu.xreg) }
-func addrImmediate(cpu *CPU6502) uint16 { return cpu.pc - 1 }
-func addrRelative(cpu *CPU6502) uint16  { return cpu.pc - 1 }
-func addrImplied(cpu *CPU6502) uint16   { return 0 }
-func addrAccum(cpu *CPU6502) uint16     { return 0 }
-
 // Opcode implementations
 func opAdc(cpu *CPU6502, arg uint16 /*int*/) int64 { return 0 }
 
@@ -45,7 +16,6 @@ func opAnd(cpu *CPU6502, arg uint16 /*int*/) int64 {
 	cpu.negFlagNote = cpu.areg
 	cpu.zeroFlagNote = cpu.areg
 	return 0
-
 }
 
 func opAslm(cpu *CPU6502, arg uint16 /*int*/) int64 { return 0 }
@@ -182,26 +152,96 @@ func opClv(cpu *CPU6502, arg uint16) int64 {
 func opCmp(cpu *CPU6502, arg uint16 /*int*/) int64 { return 0 }
 func opCpx(cpu *CPU6502, arg uint16 /*int*/) int64 { return 0 }
 func opCpy(cpu *CPU6502, arg uint16 /*int*/) int64 { return 0 }
-func opDec(cpu *CPU6502, arg uint16) int64         { return 0 }
-func opDex(cpu *CPU6502, arg uint16) int64         { return 0 }
-func opDey(cpu *CPU6502, arg uint16) int64         { return 0 }
-func opEor(cpu *CPU6502, arg uint16 /*int*/) int64 { return 0 }
-func opInc(cpu *CPU6502, arg uint16 /*int*/) int64 { return 0 }
-func opInx(cpu *CPU6502, arg uint16) int64         { return 0 }
-func opIny(cpu *CPU6502, arg uint16) int64         { return 0 }
+
+func opDec(cpu *CPU6502, arg uint16) int64 {
+	val := cpu.mem.Read(arg, cpu.cycle)
+	val--
+	cpu.mem.Write(arg, val, cpu.cycle)
+	cpu.negFlagNote = val
+	cpu.zeroFlagNote = val
+	return 0
+}
+
+func opDex(cpu *CPU6502, arg uint16) int64 {
+	cpu.xreg--
+	cpu.negFlagNote = cpu.xreg
+	cpu.zeroFlagNote = cpu.xreg
+	return 0
+}
+
+func opDey(cpu *CPU6502, arg uint16) int64 {
+	cpu.yreg++
+	cpu.negFlagNote = cpu.yreg
+	cpu.zeroFlagNote = cpu.yreg
+	return 0
+}
+
+func opEor(cpu *CPU6502, arg uint16 /*int*/) int64 {
+	val := cpu.mem.Read(arg, cpu.cycle)
+	cpu.areg ^= val
+	cpu.negFlagNote = cpu.areg
+	cpu.zeroFlagNote = cpu.areg
+	return 0
+}
+
+func opInc(cpu *CPU6502, arg uint16 /*int*/) int64 {
+	val := cpu.mem.Read(arg, cpu.cycle)
+	val++
+	cpu.mem.Write(arg, val, cpu.cycle)
+	cpu.negFlagNote = val
+	cpu.zeroFlagNote = val
+	return 0
+}
+
+func opInx(cpu *CPU6502, arg uint16) int64 {
+	cpu.xreg++
+	cpu.negFlagNote = cpu.xreg
+	cpu.zeroFlagNote = cpu.xreg
+	return 0
+}
+
+func opIny(cpu *CPU6502, arg uint16) int64 {
+	cpu.yreg++
+	cpu.negFlagNote = cpu.yreg
+	cpu.zeroFlagNote = cpu.yreg
+	return 0
+}
 
 func opJmp(cpu *CPU6502, arg uint16 /*int*/) int64 {
 	cpu.pc = arg
 	return 0
 }
 
-func opJsr(cpu *CPU6502, arg uint16 /*int*/) int64  { return 0 }
-func opLda(cpu *CPU6502, arg uint16 /*int*/) int64  { return 0 }
-func opLdx(cpu *CPU6502, arg uint16 /*int*/) int64  { return 0 }
-func opLdy(cpu *CPU6502, arg uint16 /*int*/) int64  { return 0 }
+func opJsr(cpu *CPU6502, arg uint16 /*int*/) int64 { return 0 }
+
+func opLda(cpu *CPU6502, arg uint16 /*int*/) int64 {
+	cpu.areg = cpu.mem.Read(arg, cpu.cycle)
+	cpu.negFlagNote = cpu.areg
+	cpu.zeroFlagNote = cpu.areg
+	return 0
+}
+
+func opLdx(cpu *CPU6502, arg uint16 /*int*/) int64 {
+	cpu.xreg = cpu.mem.Read(arg, cpu.cycle)
+	cpu.negFlagNote = cpu.xreg
+	cpu.zeroFlagNote = cpu.xreg
+	return 0
+}
+
+func opLdy(cpu *CPU6502, arg uint16 /*int*/) int64 {
+	cpu.yreg = cpu.mem.Read(arg, cpu.cycle)
+	cpu.negFlagNote = cpu.yreg
+	cpu.zeroFlagNote = cpu.yreg
+	return 0
+}
+
 func opLsra(cpu *CPU6502, arg uint16) int64         { return 0 }
 func opLsrm(cpu *CPU6502, arg uint16 /*int*/) int64 { return 0 }
-func opNop(cpu *CPU6502, arg uint16) int64          { return 0 }
+
+func opNop(cpu *CPU6502, arg uint16) int64 {
+	return 0
+}
+
 func opOra(cpu *CPU6502, arg uint16 /*int*/) int64 {
 	val := cpu.mem.Read(arg, cpu.cycle)
 	cpu.areg |= val
@@ -209,6 +249,7 @@ func opOra(cpu *CPU6502, arg uint16 /*int*/) int64 {
 	cpu.zeroFlagNote = cpu.areg
 	return 0
 }
+
 func opPha(cpu *CPU6502, arg uint16) int64          { return 0 }
 func opPhp(cpu *CPU6502, arg uint16) int64          { return 0 }
 func opPla(cpu *CPU6502, arg uint16) int64          { return 0 }
@@ -220,10 +261,12 @@ func opRorm(cpu *CPU6502, arg uint16 /*int*/) int64 { return 0 }
 func opRti(cpu *CPU6502, arg uint16) int64          { return 0 }
 func opRts(cpu *CPU6502, arg uint16) int64          { return 0 }
 func opSbc(cpu *CPU6502, arg uint16 /*int*/) int64  { return 0 }
+
 func opSec(cpu *CPU6502, arg uint16) int64 {
 	cpu.status.Carry = true
 	return 0
 }
+
 func opSed(cpu *CPU6502, arg uint16) int64 {
 	cpu.status.Dec = true
 	return 0
@@ -233,12 +276,59 @@ func opSei(cpu *CPU6502, arg uint16) int64 {
 	return 0
 }
 
-func opSta(cpu *CPU6502, arg uint16 /*int*/) int64 { return 0 }
-func opStx(cpu *CPU6502, arg uint16 /*int*/) int64 { return 0 }
-func opSty(cpu *CPU6502, arg uint16 /*int*/) int64 { return 0 }
-func opTax(cpu *CPU6502, arg uint16) int64         { return 0 }
-func opTay(cpu *CPU6502, arg uint16) int64         { return 0 }
-func opTsx(cpu *CPU6502, arg uint16) int64         { return 0 }
-func opTxa(cpu *CPU6502, arg uint16) int64         { return 0 }
-func opTxs(cpu *CPU6502, arg uint16) int64         { return 0 }
-func opTya(cpu *CPU6502, arg uint16) int64         { return 0 }
+func opSta(cpu *CPU6502, arg uint16 /*int*/) int64 {
+	cpu.mem.Write(arg, cpu.areg, cpu.cycle)
+	return 0
+}
+
+func opStx(cpu *CPU6502, arg uint16 /*int*/) int64 {
+	cpu.mem.Write(arg, cpu.xreg, cpu.cycle)
+	return 0
+}
+
+func opSty(cpu *CPU6502, arg uint16 /*int*/) int64 {
+	cpu.mem.Write(arg, cpu.yreg, cpu.cycle)
+	return 0
+}
+
+func opTax(cpu *CPU6502, arg uint16) int64 {
+	cpu.xreg = cpu.areg
+	cpu.negFlagNote = cpu.areg
+	cpu.zeroFlagNote = cpu.areg
+	return 0
+}
+
+func opTay(cpu *CPU6502, arg uint16) int64 {
+	cpu.yreg = cpu.areg
+	cpu.negFlagNote = cpu.areg
+	cpu.zeroFlagNote = cpu.areg
+	return 0
+}
+
+func opTsx(cpu *CPU6502, arg uint16) int64 {
+	cpu.xreg = cpu.spreg
+	cpu.negFlagNote = cpu.spreg
+	cpu.zeroFlagNote = cpu.spreg
+	return 0
+}
+
+func opTxa(cpu *CPU6502, arg uint16) int64 {
+	cpu.areg = cpu.xreg
+	cpu.negFlagNote = cpu.xreg
+	cpu.zeroFlagNote = cpu.xreg
+	return 0
+}
+
+func opTxs(cpu *CPU6502, arg uint16) int64 {
+	cpu.spreg = cpu.xreg
+	cpu.negFlagNote = cpu.xreg
+	cpu.zeroFlagNote = cpu.xreg
+	return 0
+}
+
+func opTya(cpu *CPU6502, arg uint16) int64 {
+	cpu.areg = cpu.yreg
+	cpu.negFlagNote = cpu.yreg
+	cpu.zeroFlagNote = cpu.yreg
+	return 0
+}
