a0143b43f97536122ea5cbc9bcef296892a45b88
khedoros <khedoros@gmail.com>
2022-06-25 00:48:25 -0600

    Initial attempt at MMC3
    - It's not correct, but boots some games with corrupted graphics
    - IRQ not implemented at all

--- NesCart/NesMappers/mmc3-004.go
+++ NesCart/NesMappers/mmc3-004.go
@@ -1,24 +1,112 @@
 package mappers
 
+import "fmt"
+
 type Mmc3Mapper struct {
 	prgROM uint
 	chrROM uint
+
+	chrAddrXor    uint  // either 0 or 0x1000, set by 8000 b 7
+	prgLowIsFixed bool  // 1=0x8000-0x9fff is fixed, 0=0xc000-dfff is fixed, 8000 b6
+	ctrlCmd       uint8 // value is 0-7, to specify 1 of 8 commands. 8000 b0-2
+
+	mirror       MirrorType // a000 b0, 0=vert, 1=horiz
+	sRamEnable   bool       // a001 b7
+	sRamReadOnly bool       // a001 b6
+
+	lowPrgPage uint // 0x8000-0x9fff
+	midPrgPage uint // 0xa000-0xbfff
+	hiPrgPage  uint // 0xc000-0xdfff
+
+	chrPages [8]uint // 8 pages, 0x400 each
+
 }
 
 func (m Mmc3Mapper) MapCpu(addr uint16, cycle uint64) uint {
-	return (uint(addr) - 0x8000) % m.prgROM
+	if addr >= 0x8000 && addr <= 0x9fff {
+		return (uint(addr) - 0x8000 + m.lowPrgPage*8192) % m.prgROM
+	} else if addr >= 0xa000 && addr <= 0xbfff {
+		return (uint(addr) - 0xa000 + m.midPrgPage*8192) % m.prgROM
+	} else if addr >= 0xc000 && addr <= 0xdfff {
+		return (uint(addr) - 0xc000 + m.hiPrgPage*8192) % m.prgROM
+	} else if addr >= 0xe000 && addr <= 0xffff {
+		return (uint(addr) - 0xe000 + m.prgROM - 8192) % m.prgROM
+	}
+	return 0
 }
 
 func (m Mmc3Mapper) MapPpu(addr uint16, cycle uint64) uint {
-	return uint(addr)
+	return m.chrPages[addr>>10]*0x400 + uint(addr&0x3ff) ^ m.chrAddrXor
 }
 
-func (m *Mmc3Mapper) WriteCpu(addr uint16, val uint8, cycle uint64) {}
+func (m *Mmc3Mapper) WriteCpu(addr uint16, val uint8, cycle uint64) {
+	addr &= 0xe001
+	switch addr {
+	case 0x8000: // ctrl1 register
+		m.chrAddrXor = 32 * uint(val&0x80)
+		m.prgLowIsFixed = val&0x40 == 0x40
+		m.ctrlCmd = val & 0x7
+
+		/*
+			000b 0 - Select 2 1K CHR ROM pages at 0000h in PPU space
+			001b 1 - Select 2 1K CHR ROM pages at 0800h in PPU space
+			010b 2 - Select 1K CHR ROM page at 1000h in PPU space
+			011b 3 - Select 1K CHR ROM page at 1400h in PPU space
+			100b 4 - Select 1K CHR ROM page at 1800h in PPU space
+			101b 5 - Select 1K CHR ROM page at 1C00h in PPU space
+			110b 6 - Select 8K PRG ROM page at 8000h or C000h
+			111b 7 - Select 8K PRG ROM page at A000h
+		*/
+	case 0x8001: // bank for ctrl1
+		switch m.ctrlCmd {
+		case 0:
+			m.chrPages[0] = uint(val)
+			m.chrPages[1] = uint(val + 1)
+		case 1:
+			m.chrPages[2] = uint(val)
+			m.chrPages[3] = uint(val + 1)
+		case 2:
+			m.chrPages[4] = uint(val)
+		case 3:
+			m.chrPages[5] = uint(val)
+		case 4:
+			m.chrPages[6] = uint(val)
+		case 5:
+			m.chrPages[7] = uint(val)
+		case 6:
+			if m.prgLowIsFixed {
+				m.hiPrgPage = uint(val)
+			} else {
+				m.lowPrgPage = uint(val)
+			}
+		case 7:
+			m.midPrgPage = uint(val)
+		}
+	case 0xa000:
+		m.mirror = MirrorType(val & 0x1)
+	case 0xa001:
+		m.sRamEnable = val&0x80 == 0x80
+		m.sRamReadOnly = val&0x40 == 0x40
+	case 0xc000:
+		fmt.Println("MMC3 0xc000 IRQ not implemented")
+	case 0xc001:
+		fmt.Println("MMC3 0xc001 IRQ not implemented")
+	case 0xe000:
+		fmt.Println("MMC3 0xe000 IRQ not implemented")
+	case 0xe001:
+		fmt.Println("MMC3 0xe001 IRQ not implemented")
+	}
+}
 
 func (m *Mmc3Mapper) New(prg, chr uint) {
 	m.prgROM, m.chrROM = prg, chr
+	m.lowPrgPage, m.midPrgPage, m.hiPrgPage = 0, 1, (m.prgROM/8192)-2
+	m.prgLowIsFixed = false
+	m.sRamEnable = false
+	m.sRamReadOnly = true
+	m.chrPages = [8]uint{0, 1, 2, 3, 4, 5, 6, 7}
 }
 
 func (m *Mmc3Mapper) GetMirror() MirrorType {
-	return HARDWIRED
+	return m.mirror
 }
